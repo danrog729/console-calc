658b01f34748845b256a94df4d1fd5cf09360539
Dan Rogers <danrog729@gmail.com>
2024-09-16 21:47:35 +0100

    added freeform calc

--- console-calc/Program.cs
+++ console-calc/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -193,8 +194,8 @@ namespace console_calc
                     case 18:
                         {
                             // hex -> bin
-                            string binary = UserInputHex("Enter hexadecimal value: 0x");
-                            Console.WriteLine("Binary: 0b" + Convert.ToString(Convert.ToInt32(binary, 16),2));
+                            string hex = UserInputHex("Enter hexadecimal value: 0x");
+                            Console.WriteLine("Binary: 0b" + Convert.ToString(Convert.ToInt32(hex, 16),2));
                             break;
                         }
                     case 19:
@@ -206,9 +207,9 @@ namespace console_calc
                         }
                     case 20:
                         {
-                            // hex -> dec
-                            string binary = UserInputHex("Enter hexadecimal value: 0x");
-                            Console.WriteLine("Denary: " + Convert.ToInt32(binary, 16));
+                            // hex -> den
+                            string hex = UserInputHex("Enter hexadecimal value: 0x");
+                            Console.WriteLine("Denary: " + Convert.ToInt32(hex, 16));
                             break;
                         }
                     case 21:
@@ -223,6 +224,7 @@ namespace console_calc
                                 string expression = Console.ReadLine();
                                 calc.Input = expression;
                             }
+                            Console.WriteLine("Result: " + calc.Evaluate());
 
                             break;
                         }
@@ -311,48 +313,52 @@ namespace console_calc
 
     public class FreeformCalculator
     {
-        private string input;
+        private string _input;
         public string Input
         {
-            get { return input; }
+            get { return _input; }
             set
             {
-                input = value;
+                _input = value;
                 Tokenise();
+                CorrectTokens();
                 ShuntingYard();
             }
         }
-        private Token[] tokenisedInput;
-        private Stack<Token> expression;
+        private List<Token> tokenList;
+        private Queue<Token> expression;
         private HashSet<char> operators;
         public bool isValidExpression;
 
         public FreeformCalculator()
         {
-            input = "";
-            expression = new Stack<Token>();
-            tokenisedInput = new Token[0];
+            _input = "";
+            expression = new Queue<Token>();
+            tokenList = new List<Token>();
             isValidExpression = false;
             operators = new HashSet<char> {'+', '-', '*', '/', '^', '(', ')'};
         }
 
         private void Tokenise()
         {
-            List<Token> tokenList = new List<Token>();
+            tokenList = new List<Token>();
+
+            _input = _input.Replace(" ", "");
+            _input = _input.Replace(",", "");
 
             int tokenStart = 0;
             int tokenEnd = 0;
             bool hasDecimal = false;
 
-            for (int index = 0; index < input.Length; index++)
+            for (int index = 0; index < _input.Length; index++)
             {
-                if (operators.Contains(input[index]))
+                if (operators.Contains(_input[index]))
                 {
                     // if the character is an operator
                     tokenEnd = index;
 
                     // tokenise
-                    switch (input[index])
+                    switch (_input[index])
                     {
                         case '+':
                             tokenList.Add(new AdditionToken()); break;
@@ -370,20 +376,21 @@ namespace console_calc
                             tokenList.Add(new CloseBracketToken()); break;
                         default:
                             isValidExpression = false;
-                            Console.WriteLine("ERROR | No token for operator: \"" + input[index] + "\"");
+                            Console.WriteLine("ERROR | No token for operator: \"" + _input[index] + "\"");
                             return;
                     }
 
                     // start the next token
                     tokenStart = index + 1;
                 }
-                else if (input[index] == '.')
+                else if (_input[index] == '.')
                 {
                     // if the character is a decimal point
                     if (hasDecimal)
                     {
                         // ERROR
                         isValidExpression = false;
+                        Console.WriteLine("ERROR | A number cannot have two decimal points!");
                         return;
                     }
                     else
@@ -391,14 +398,14 @@ namespace console_calc
                         hasDecimal = true;
                     }
                 }
-                if (Char.IsDigit(input[index]) || input[index] == '.')
+                if (Char.IsDigit(_input[index]) || _input[index] == '.')
                 {
                     // if the character is a decimal point or a digit
-                    if (index == input.Length - 1 || !(Char.IsDigit(input[index + 1]) || (input[index + 1] == '.')))
+                    if (index == _input.Length - 1 || !(Char.IsDigit(_input[index + 1]) || (_input[index + 1] == '.')))
                     {
                         // end of the number
                         // numbers cant end with a decimal point
-                        if (input[index] == '.')
+                        if (_input[index] == '.')
                         {
                             isValidExpression = false;
                             return;
@@ -406,7 +413,7 @@ namespace console_calc
                         tokenEnd = index;
 
                         // tokenise
-                        string inputSubstring = input.Substring(tokenStart, tokenEnd - tokenStart + 1);
+                        string inputSubstring = _input.Substring(tokenStart, tokenEnd - tokenStart + 1);
                         if (!Single.TryParse(inputSubstring, out float inputFloat))
                         {
                             Console.WriteLine("ERROR | Could not parse \"" + inputSubstring + "\" as a float.");
@@ -421,23 +428,139 @@ namespace console_calc
                     }
                 }
             }
+        }
 
-            // tokenised now
-            // convert to an array
-            tokenisedInput = new Token[tokenList.Count];
-            for (int index = 0; index < tokenList.Count; index++)
+        private void CorrectTokens()
+        {
+            int index = 0;
+            while (index < tokenList.Count)
             {
-                tokenisedInput[index] = tokenList[index];
+                if (tokenList[index] is SubtractionToken)
+                {
+                    if (index == 0 || tokenList[index-1] is OperatorToken & !(tokenList[index-1] is CloseBracketToken))
+                    {
+                        tokenList.Remove(tokenList[index]);
+                        if (tokenList[index] is FloatToken)
+                        {
+                            ((FloatToken)tokenList[index]).value *= -1;
+                        }
+                    }
+                }
+                else if (tokenList[index] is OpenBracketToken)
+                {
+                    if (index != 0)
+                    {
+                        if (tokenList[index - 1] is CloseBracketToken || !(tokenList[index - 1] is OperatorToken))
+                        {
+                            // insert a mult token before the open bracket
+                            tokenList.Insert(index++, new MultiplicationToken());
+                        }
+                    }
+                }
+                else if (tokenList[index] is CloseBracketToken)
+                {
+                    if (index != tokenList.Count - 1)
+                    {
+                        if (tokenList[index + 1] is OpenBracketToken || !(tokenList[index + 1] is OperatorToken))
+                        {
+                            // insert a mult token after the close bracket
+                            tokenList.Insert(++index, new MultiplicationToken());
+                        }
+                    }
+                }
+
+                index++;
             }
         }
 
         private void ShuntingYard()
         {
+            expression = new Queue<Token>();
+            Stack<Token> operatorStack = new Stack<Token>();
 
+            foreach (Token token in tokenList)
+            {
+                if (!(token is OperatorToken))
+                {
+                    // enqueue any floats to the queue
+                    expression.Enqueue(token);
+                }
+                else if (token is CloseBracketToken)
+                {
+                    // enqueue operators from the stack to the queue until an open bracket is reached
+                    bool reachedOpenBracket = false;
+                    while (!reachedOpenBracket)
+                    {
+                        Token opToken = operatorStack.Pop();
+                        if (opToken is OpenBracketToken)
+                        {
+                            reachedOpenBracket = true;
+                        }
+                        else
+                        {
+                            expression.Enqueue(opToken);
+                        }
+                    }
+                }
+                else if (!(token is OpenBracketToken))
+                {
+                    // enqueue operators from the stack to the queue until the stack's precedence is equal
+                    bool shouldPop = true;
+                    while (operatorStack.Count > 0 && shouldPop)
+                    {
+                        if (((OperatorToken)(operatorStack.Peek())).precedence <= ((OperatorToken)token).precedence)
+                        {
+                            expression.Enqueue(operatorStack.Pop());
+                        }
+                        else
+                        {
+                            shouldPop = false;
+                        }
+                    }
+                    operatorStack.Push(token);
+                }
+                else
+                {
+                    operatorStack.Push(token);
+                }
+            }
+
+            // clear the remains of the operator stack
+            while (operatorStack.Count > 0)
+            {
+                expression.Enqueue(operatorStack.Pop());
+            }
+
+            isValidExpression = true;
+        }
+
+        public float Evaluate()
+        {
+            Stack<FloatToken> workingStack = new Stack<FloatToken>();
+            while (expression.Count > 0)
+            {
+                if (expression.Peek() is OperatorToken)
+                {
+                    OperatorToken opToken = (OperatorToken)expression.Dequeue();
+                    if (workingStack.Count < 2)
+                    {
+                        Console.WriteLine("ERROR | Invalid expression.");
+                        return 0f;
+                    }
+                    float secondArg = workingStack.Pop().value;
+                    float firstArg = workingStack.Pop().value;
+                    workingStack.Push(new FloatToken(opToken.Operate(firstArg, secondArg)));
+                }
+                else
+                {
+                    workingStack.Push((FloatToken)expression.Dequeue());
+                }
+            }
+            return workingStack.Peek().value;
         }
     }
 
-    public class Token
+    public abstract class Token
     {
 
     }
@@ -450,63 +573,137 @@ namespace console_calc
         {
             value = newValue;
         }
+
+        public override string ToString()
+        {
+            return "<" + value.ToString() + ">";
+        }
     }
 
-    public class OperatorToken : Token
+    public abstract class OperatorToken : Token
     {
-        public float Operate(float value1, float value2)
-        {
-            return 0f;
-        }
+        public int precedence;
+        public abstract float Operate(float value1, float value2);
     }
 
     public class AdditionToken : OperatorToken
     {
-        new public float Operate(float value1, float value2)
+        public AdditionToken()
+        {
+            precedence = 3;
+        }
+        public override float Operate(float value1, float value2)
         {
             return value1 + value2;
+        }
+
+        public override string ToString()
+        {
+            return "<+>";
         }
     }
 
     public class SubtractionToken : OperatorToken
     {
-        new public float Operate(float value1, float value2)
+        public SubtractionToken()
+        {
+            precedence = 3;
+        }
+        public override float Operate(float value1, float value2)
         {
             return value1 - value2;
+        }
+
+        public override string ToString()
+        {
+            return "<->";
         }
     }
 
     public class MultiplicationToken : OperatorToken
     {
-        new public float Operate(float value1, float value2)
+        public MultiplicationToken()
+        {
+            precedence = 2;
+        }
+        public override float Operate(float value1, float value2)
         {
             return value1 * value2;
+        }
+
+        public override string ToString()
+        {
+            return "<*>";
         }
     }
 
     public class DivisionToken : OperatorToken
     {
-        new public float Operate(float value1, float value2)
+        public DivisionToken()
+        {
+            precedence = 2;
+        }
+        public override float Operate(float value1, float value2)
         {
             return value1 / value2;
+        }
+
+        public override string ToString()
+        {
+            return "</>";
         }
     }
 
     public class ExponentiationToken : OperatorToken
     {
-        new public float Operate(float value1, float value2)
+        public ExponentiationToken()
+        {
+            precedence = 1;
+        }
+        public override float Operate(float value1, float value2)
         {
             return (float)Math.Pow(value1,value2);
+        }
+
+        public override string ToString()
+        {
+            return "<^>";
         }
     }
 
     public class OpenBracketToken : OperatorToken
     {
+        public OpenBracketToken()
+        {
+            precedence = 999;
+        }
 
+        public override float Operate(float value1, float value2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "<(>";
+        }
     }
 
     public class CloseBracketToken : OperatorToken
     {
+        public CloseBracketToken()
+        {
+            precedence = 999;
+        }
 
+        public override float Operate(float value1, float value2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "<)>";
+        }
     }
 }
